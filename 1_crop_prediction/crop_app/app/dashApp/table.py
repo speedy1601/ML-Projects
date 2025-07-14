78f0a2314e7e3313b3beaba76f856820fdd7215d
Hasibul Habib <hasibulhabib1601@gmail.com>
2025-07-15 05:34:19 +0600

    replaced polars with pandas

--- 1_crop_prediction/crop_app/app/dashApp/table.py
+++ 1_crop_prediction/crop_app/app/dashApp/table.py
@@ -1,11 +1,11 @@
-import polars as pl
+import pandas as pd
 from dash import Dash, html, dash_table
 import os
 
 cur_folder = os.path.dirname(os.path.realpath(__file__))
 DATASET_PATH = os.path.join(cur_folder, "unique_crops_sample.csv")
 
-crops:pl.DataFrame = pl.read_csv(source = DATASET_PATH).sort(by='crop_name')
+crops = pd.read_csv(DATASET_PATH) .sort_values(by='crop_name')
 
 app = Dash(__name__, requests_pathname_prefix="/dashboard/")
 
@@ -13,7 +13,7 @@ app.layout = html.Div(
     id = 'dashApp',
     children = [
         html.H1(id = 'firstH1', children = "Sample Data Table for 22 unique crops"),
-        dash_table.DataTable(id = 'table1', data = crops.to_pandas().to_dict(orient = 'records')),
+        dash_table.DataTable(id = 'table1', data = crops.to_dict(orient = 'records')),
     ],
 )
 
